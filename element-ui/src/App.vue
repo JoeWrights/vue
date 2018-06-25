ee0b164ffa44da8de402aeff3f5f0cf5be2e0ed4
JoeWrights <1035208578@qq.com>
2018-06-25 23:52:07 +0800

    element-ui source code study

---
+++ element-ui/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-button :disabled="disabled" type="success" loading>提交</el-button>
+    <el-button :disabled="disabled" type="primary" :loading="isLoading" icon="el-icon-edit" @click="doLoading">提交</el-button>
+  </div>
+</template>
+
+<script>
+import ElButton from '@/components/ElButton';
+export default {
+  name: 'App',
+  components: {
+    "el-button":ElButton
+  },
+  data(){
+    return {
+      disabled:false,
+      // type:"success"
+      isLoading:false
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.disabled=true;
+    },2000)
+  },
+  methods(){
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ element-ui/src/components/ElButton.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <button class="el-button" :disabled="buttonDisabled" :class="[type?'el-button--'+ type:'',{'is-disabled':'buttonDisabled','is-loading':'loading'}]">
+        <i class="el-icon-loading" v-if="loading"></i>
+        <i v-if="icon&&!loading" :class="icon"></i>
+        <slot></slot></button>
+</template>
+
+<script>
+export default {
+    name:"ElButton",   //声明组件
+    props: {
+        disabled:Boolean,
+        type: {
+            type: String,
+            default: "default"
+        },
+        loading:Boolean,
+        icon: {
+            icon: String,
+            default: ""
+        }
+    },
+    computed: {
+        buttonDisabled(){
+            return this.disabled;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
