9bd4149c626b5bb6fa0fdd83083770c4ae1d31d2
JoeWrights <1035208578@qq.com>
2018-07-18 23:35:58 +0800

    学习项目vue-zhihu

---
+++ vue-zhihu/src/api/config.js
@@ -0,0 +1,6 @@
+const config = {
+  baseURL: 'https://zhihu-agent.herokuapp.com',
+  method: '/get?api='
+}
+export const API_ROOT = config.baseURL.concat(config.method);
+
---
+++ vue-zhihu/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import api from '@/api/request'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    api.getNews().then((data) => {
+      console.log(data);
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
