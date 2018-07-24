6f85e7495996f742f1c0025a2b580c95b284f60a
JoeWrights <1035208578@qq.com>
2018-07-24 23:25:32 +0800

    update request文件

--- vue-zhihu/src/api/request.js
+++ vue-zhihu/src/api/request.js
@@ -9,37 +9,50 @@ import {
   SectionIdResource
 } from './resouce'
 import axios from 'axios'
-export default {
-  getNews() {
-    return axios.get(NewsResource);
-  },
-  getNewsById(id) {
-    return axios.get(NewsIdResource + id)
-  },
-  getNewsByDate(date) {
-    return axios.get(NewsDateResource + date)
-  },
-  getNewsInfoById(id) {
-    return axios.get(NewsInfoResource, {
-      params: {
-        id: id
-      }
-    })
-  },
-  getTopics() {
-    return axios.get(TopicsResource)
-  },
-  getTopicsById(topicid) {
-    return axios.get(TopicsIdResource + topicid)
-  },
-  getSections() {
-    return axios.get(SectionsResource)
-  },
-  getSectionsById(sectionid) {
-    return axios.get(SectionIdResource, {
-      params: {
-        sectionid: sectionid
-      }
-    })
-  }
-}
+// export default {
+//   getNews() {
+//     // return axios.get(NewsResource);
+//     return new Promise((resolve, reject) => {
+//       axios.get(NewsResource).then(res => res.data).then(data => {
+//         resolve(data);
+//       });
+//     });
+//   },
+//   getNewsById(id) {
+//     return axios.get(NewsIdResource + id)
+//   },
+//   getNewsByDate(date) {
+//     return axios.get(NewsDateResource + date)
+//   },
+//   getNewsInfoById(id) {
+//     return axios.get(NewsInfoResource, {
+//       params: {
+//         id: id
+//       }
+//     })
+//   },
+//   getTopics() {
+//     return axios.get(TopicsResource)
+//   },
+//   getTopicsById(topicid) {
+//     return axios.get(TopicsIdResource + topicid)
+//   },
+//   getSections() {
+//     return axios.get(SectionsResource)
+//   },
+//   getSectionsById(sectionid) {
+//     return axios.get(SectionIdResource, {
+//       params: {
+//         sectionid: sectionid
+//       }
+//     })
+//   }
+// }
+export const getNews = () => new Promise((resolve, reject) => {
+  axios.get(NewsResource)
+  .then(res => res.data)
+  .then(data => resolve(data))
+  .catch(err => {
+    reject(err);
+  });
+});
